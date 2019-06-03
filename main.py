d835499f59d405c0911e9f3d18afb79bcdbb5efa
PaulN07 <JamesPerisher@users.noreply.github.com>
2019-06-04 01:06:06 +1000

    fixed exec command

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ class caller:
     def __init__(self, perms, col):
         self.perms = perms
         self.col = col
-        self.all_exec = []
+        self.all_exec = {}
         self.exec = None
 
     def add_exec(self, new):
-        self.all_exec.append(new(self, self.perms))
-        self.exec = self.all_exec[0]
+        new = new(self, self.perms)
+        self.all_exec[new.name.lower()] = new
+        self.exec = self.all_exec[list(self.all_exec)[0]]
 
     def prompt(self):
         print("%s[%s]%s[%s]%s%s" %(self.col, self.perms, self.exec.col, self.exec.name, colour("white", bright=False), self.exec.get_linestat()), end="")
@@ -34,12 +35,16 @@ class caller:
 
             if check_comm[0] == "exec":
                 if check_comm[1] == "list":
-                    maxex = max(len(x) for x in [y.name for y in self.all_exec])+2
-                    maxexname = max(len(x) for x in [y.description for y in self.all_exec])
-                    out = "\n".join([("%s%s%s%s"%(x.name, " "*(maxex-len(x.name)), x.description, " "*(maxexname-len(x.description)))) for x in self.all_exec])
+                    maxex     = max(len(x) for x in [self.all_exec[y].name        for y in self.all_exec])+2
+                    maxexname = max(len(x) for x in [self.all_exec[y].description for y in self.all_exec])
+                    out = "\n".join([("%s%s%s%s"%(self.all_exec[x].name, " "*(maxex-len(self.all_exec[x].name)), self.all_exec[x].description, " "*(maxexname-len(self.all_exec[x].description)))) for x in self.all_exec])
                     print(colour("magenta", bright=False)+out+"\n")
                     continue
-                print("changing exec to: %s"%check_comm[1])
+                try:
+                    self.exec = self.all_exec[check_comm[1]]
+                    print("%sset current executer to %s\n"%(colour("green", bright=False), check_comm[1]))
+                except KeyError:
+                    print("%sno module named %s\n"%(colour("red", bright=False), check_comm[1]))
                 continue
             self.get(check_comm)
 
